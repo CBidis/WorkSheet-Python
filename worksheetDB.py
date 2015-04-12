5b2c93b305780b5caa1926798ceb257e28c79be6
Christos Charalampidis <chrigeta@hotmail.com>
2015-04-12 11:02:44 +0300

    Update worksheetDB.py

--- worksheetDB.py
+++ worksheetDB.py
@@ -2,13 +2,13 @@
 import sqlite3
 
 conn = sqlite3.connect('worksheet.db')
-print ("Opened database successfully")
+
 
 conn.execute('''CREATE TABLE Names
        (NAME  TEXT,SURNAME TEXT PRIMARY KEY)''')
         
 
-print ("Table created successfully")
+
 
 conn.execute('''CREATE TABLE Ergo
        (NAME TEXT,ERGO   TEXT,ACTIVITY TEXT,DATE  REAL PRIMARY KEY,TIME_IN  TEXT , TIME_OUT  TEXT ,HOURSDEC INT(2),HOURS TEXT)''')
